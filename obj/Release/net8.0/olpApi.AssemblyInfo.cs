3d80dbcd0228ee3b6ce90906266c2827943f422c
UpendraV1 <msunnylive@gmail.com>
2025-06-09 10:16:20 +0530

    azure

--- obj/Release/net8.0/olpApi.AssemblyInfo.cs
+++ obj/Release/net8.0/olpApi.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("olpApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156ed5050f44801a15955684dc962787d03781de")]
 [assembly: System.Reflection.AssemblyProductAttribute("olpApi")]
